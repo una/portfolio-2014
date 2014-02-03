0464200c42f0f8ad3690351b51c23214a9ca0876
Una Kravets <Una21136@yahoo.com>
2014-02-03 00:22:21 -0500

    configured slider for Fannotate

--- _site/js/scripts.js
+++ _site/js/scripts.js
@@ -3,3 +3,34 @@ $(".menu-section__toggle").on('click', function() {
 	$('.menu-section').toggleClass("on");
 	$("nav ul").toggleClass('hidden');
 });
+
+//slider
+$('.slide').each(function()
+        {
+            $('#slide-controls').append('<li></li>');
+        });
+
+$('#slide-controls li').each(function(index , item)
+        {
+            $(item).on('click' , function()
+            {
+                $('.slidesjs-pagination li a').eq(index).trigger('click');
+            })
+
+        });
+
+$(document).bind('keydown',function(e){
+	if (e.keyCode == 37) { 
+		$('.slidesjs-previous').trigger('click');
+	}
+	if (e.keyCode == 39) { 
+		$('.slidesjs-next').trigger('click');
+	}
+	
+	/* arrow keys:
+	37 - left
+	38 - up
+	39 - right
+	40 - down
+	*/
+});
