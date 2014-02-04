034a981b5195247a1504a3235e4f24e98528dc02
Una Kravets <Una21136@yahoo.com>
2014-02-03 22:09:47 -0500

    menu and links functional by keyboard only

--- js/scripts.js
+++ js/scripts.js
@@ -19,19 +19,57 @@ $('#slide-controls li').each(function(index , item)
 
         });
 
+//KEYBOARD PRESSES
 $(document).bind('keydown',function(e){
-	if (e.keyCode == 76) { 
+
+	//if menu is open, bind up & down keys
+	if ($(".menu-section__toggle").hasClass('on')) {
+		if (e.keyCode == 49) { 
+			location.href = $('.menu-section ul li:nth-child(1) a').attr('href');
+		}
+		else if (e.keyCode == 50) { 
+			location.href = $('.menu-section ul li:nth-child(2) a').attr('href');		}
+		else if (e.keyCode == 51) { 
+			location.href = $('.menu-section ul li:nth-child(3) a').attr('href');		}
+		else if (e.keyCode == 52) { 
+			location.href = $('.menu-section ul li:nth-child(4) a').attr('href');		}
+		else if (e.keyCode == 53) { 
+			window.open($('.menu-section ul li:nth-child(5) a').attr('href'));		}
+		else if (e.keyCode == 54) { 
+			window.open($('.menu-section ul li:nth-child(6) a').attr('href'));		}
+		else if (e.keyCode == 55) { 
+			window.open($('.menu-section ul li:nth-child(7) a').attr('href'));		}
+		else if (e.keyCode == 56) { 
+			window.open($('.menu-section ul li:nth-child(8) a').attr('href'));		}
+		else if (e.keyCode == 57) { 
+			window.open($('.menu-section ul li:nth-child(9) a').attr('href'));		}
+	}
+	//open menu
+	if (e.keyCode == 77) { 
+		$('.menu-section__toggle').trigger('click');
+	}
+
+	//slider with + & -
+	else if (e.keyCode == 189) { 
 		$('.slidesjs-previous').trigger('click');
 	}
-	if (e.keyCode == 82) { 
+	else if (e.keyCode == 187) { 
 		$('.slidesjs-next').trigger('click');
 	}
 
-	if (e.keyCode == 37) { 
-		location.href = $('.pagination .left a').attr('href');
+	// pagination if you're inside a case-study page
+	if ($('.cs-main').length) {
+		if (e.keyCode == 37) { 
+			location.href = $('.pagination .left a').attr('href');
+		}
+		else if (e.keyCode == 39) { 
+			location.href = $('.pagination .right a').attr('href');
+		}
 	}
-	if (e.keyCode == 39) { 
-		location.href = $('.pagination .right a').attr('href');
+
+	// ? to tell you commands
+	else if (e.keyCode == 191) { 
+		alert('m: open menu, then press 1-9 to navigate the menu, left arrow: previous project, right arrow: next project, L: prev. slide (slider), R: next slide (slider), +: next page (turn.js), -: previous page, ?: key');
 	}
 	
 	/* arrow keys:
@@ -41,5 +79,19 @@ $(document).bind('keydown',function(e){
 	40 - down
 	76 - L
 	82 - R
+	77 - M
+	187 - + =
+	189 - - _
+	191 - /?
+	16 - shift
+	1 - 49
+	2 - 50
+	3 - 51
+	4 - 52
+	5 - 53
+	6 - 54
+	7 - 55
+	8 - 56
+	9 -57
 	*/
 });
