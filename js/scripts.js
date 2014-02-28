532217647545fb0e6d3707f5c6a3545ed1a92d73
Una Kravets <Una21136@yahoo.com>
2014-02-28 16:04:43 -0500

    added proofesor things image and fixed some of the white space

--- js/scripts.js
+++ js/scripts.js
@@ -1,122 +1,122 @@
 $(".menu-section__toggle").on('click', function () {
-	$(this).toggleClass("on");
-	$('.menu-section').toggleClass("on");
-	$("nav ul").toggleClass('hidden');
+  $(this).toggleClass("on");
+  $('.menu-section').toggleClass("on");
+  $("nav ul").toggleClass('hidden');
 });
 
 //slider
 $('.slide').each(function () {
-	$('#slide-controls').append('<li></li>');
+  $('#slide-controls').append('<li></li>');
 });
 
 $('#slide-controls li').each(function (index, item) {
-	$(item).on('click', function ()
-	{
-		$('.slidesjs-pagination li a').eq(index).trigger('click');
-	});
+  $(item).on('click', function ()
+  {
+    $('.slidesjs-pagination li a').eq(index).trigger('click');
+  });
 });
 
 //KEYBOARD PRESSES
 $(document).keydown(function(e) {
-	if ($(".menu-section__toggle").hasClass('on')) {
-		switch(e.keyCode) {
-			case 49: //1
-				location.href = $('.menu-section ul li:nth-child(1) a').attr('href');
-				break;
+  if ($(".menu-section__toggle").hasClass('on')) {
+    switch(e.keyCode) {
+      case 49: //1
+        location.href = $('.menu-section ul li:nth-child(1) a').attr('href');
+        break;
 
-			case 50: //2
-				location.href = $('.menu-section ul li:nth-child(2) a').attr('href');
-				break;
+      case 50: //2
+        location.href = $('.menu-section ul li:nth-child(2) a').attr('href');
+        break;
 
-			case 51: //3
-				location.href = $('.menu-section ul li:nth-child(3) a').attr('href');
-				break;
+      case 51: //3
+        location.href = $('.menu-section ul li:nth-child(3) a').attr('href');
+        break;
 
-			case 52: //4
-				location.href = $('.menu-section ul li:nth-child(4) a').attr('href');
-				break;
+      case 52: //4
+        location.href = $('.menu-section ul li:nth-child(4) a').attr('href');
+        break;
 
-			case 53: //5
-				location.href = $('.menu-section ul li:nth-child(5) a').attr('href');
-				break;
+      case 53: //5
+        location.href = $('.menu-section ul li:nth-child(5) a').attr('href');
+        break;
 
-			default: return; // exit this handler for other keys
-			}
-		}
-		
-	//open menu
-	if (e.keyCode == 77) { 
-		$('.menu-section__toggle').trigger('click');
-	}
+      default: return; // exit this handler for other keys
+      }
+    }
+    
+  //open menu
+  if (e.keyCode == 77) { 
+    $('.menu-section__toggle').trigger('click');
+  }
 
-	//slider with + & -
-	if (e.keyCode == 189) { 
-		$('.slidesjs-previous').trigger('click');
-	}
-	if (e.keyCode == 187) { 
-		$('.slidesjs-next').trigger('click');
-	}
+  //slider with + & -
+  if (e.keyCode == 189) { 
+    $('.slidesjs-previous').trigger('click');
+  }
+  if (e.keyCode == 187) { 
+    $('.slidesjs-next').trigger('click');
+  }
 
-	// pagination if you're inside a case-study page
-	if ($('.cs-main').length) {
-		if (e.keyCode == 37) { 
-			location.href = $('.pagination .left a').attr('href');
-		}
-		else if (e.keyCode == 39) { 
-			location.href = $('.pagination .right a').attr('href');
-		}
-	}
+  // pagination if you're inside a case-study page
+  if ($('.cs-main').length) {
+    if (e.keyCode == 37) { 
+      location.href = $('.pagination .left a').attr('href');
+    }
+    else if (e.keyCode == 39) { 
+      location.href = $('.pagination .right a').attr('href');
+    }
+  }
 
-	// ? to tell you commands
-	if (e.keyCode == 191) { 
-	alert('m: open menu, then press 1-9 to navigate the menu, left arrow: previous project, right arrow: next project, L: prev. slide (slider), R: next slide (slider), +: next page (turn.js), -: previous page, ?: key');
-	}
-	
-	/* arrow keys:
-	37 - left
-	38 - up
-	39 - right
-	40 - down
-	76 - L
-	82 - R
-	77 - M
-	187 - + =
-	189 - - _
-	191 - /?
-	16 - shift
-	1 - 49
-	2 - 50
-	3 - 51
-	4 - 52
-	5 - 53
-	6 - 54
-	7 - 55
-	8 - 56
-	9 -57
-	*/
+  // ? to tell you commands
+  if (e.keyCode == 191) { 
+  alert('m: open menu, then press 1-9 to navigate the menu, left arrow: previous project, right arrow: next project, L: prev. slide (slider), R: next slide (slider), +: next page (turn.js), -: previous page, ?: key');
+  }
+  
+  /* arrow keys:
+  37 - left
+  38 - up
+  39 - right
+  40 - down
+  76 - L
+  82 - R
+  77 - M
+  187 - + =
+  189 - - _
+  191 - /?
+  16 - shift
+  1 - 49
+  2 - 50
+  3 - 51
+  4 - 52
+  5 - 53
+  6 - 54
+  7 - 55
+  8 - 56
+  9 -57
+  */
 });
 
 // WAYPOINTS
 $('.js-fadeIn').waypoint(function() {
-	$(this).css('opacity', '1');
+  $(this).css('opacity', '1');
 }, { offset: '25%' });
 
 //detect Chrome to fix ampersand bug bc firefox doesn't support unicode-range
 if(/chrom(e|ium)/.test(navigator.userAgent.toLowerCase())){
-	$('html').addClass('chrome');
+  $('html').addClass('chrome');
 }
 
 // FANNOTATE 
 $('.fannouts').waypoint(function() {
-	$(this).css('opacity', '1').addClass('opened');
+  $(this).css('opacity', '1').addClass('opened');
 }, { offset: '25%' });
 $('.js-invert').waypoint(function() {
-	$(this).addClass('inverted');
+  $(this).addClass('inverted');
 }, { offset: '50%' });
 
 // Lazy Load 
 $("img.lazy").lazyload({
-	effect : "fadeIn",
-	threshold : 200,
-	skip_invisible : false
+  effect : "fadeIn",
+  threshold : 200,
+  skip_invisible : false
 });
