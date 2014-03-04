87d7bca2fceedfa02dede8fb27bdcbc70460be30
Una Kravets <Una21136@yahoo.com>
2014-03-04 10:26:12 -0500

    refactoring js code

--- _site/js/scripts.js
+++ _site/js/scripts.js
@@ -1,107 +1,124 @@
-$(".menu-section__toggle").on('click', function () {
-  $(this).toggleClass("on");
-  $('.menu-section').toggleClass("on");
-  $("nav ul").toggleClass('hidden');
-});
+window.PORT || (PORT = {});
 
-//slider
-$('.slide').each(function () {
-  $('#slide-controls').append('<li></li>');
-});
+(function ($, window, document, undefined) {
 
-$('#slide-controls li').each(function (index, item) {
-  $(item).on('click', function ()
-  {
-    $('.slidesjs-pagination li a').eq(index).trigger('click');
-  });
-});
+  var _common = PORT.common = {
+    init: function () {
+      this.setVars();
+      this.bindEvents();
+    },
 
-//KEYBOARD PRESSES
-$(document).keydown(function(e) {
-  if ($(".menu-section__toggle").hasClass('on')) {
-    switch(e.keyCode) {
-      case 49: //1
-        location.href = $('.menu-section ul li:nth-child(1) a').attr('href');
-        break;
+    setVars: function () {
 
-      case 50: //2
-        location.href = $('.menu-section ul li:nth-child(2) a').attr('href');
-        break;
+    },
 
-      case 51: //3
-        location.href = $('.menu-section ul li:nth-child(3) a').attr('href');
-        break;
+    bindEvents: function () {
+       $(".menu-section__toggle").on('click', this.menuSection);
+       $(document).on('keydown', this.keyDown);
+    },
 
-      case 52: //4
-        location.href = $('.menu-section ul li:nth-child(4) a').attr('href');
-        break;
+    menuSection: function () {
+      $('.menu-section__toggle, .menu-section').toggleClass("on");
+      $("nav ul").toggleClass('hidden');
+    },
 
-      case 53: //5
-        location.href = $('.menu-section ul li:nth-child(5) a').attr('href');
-        break;
+    keyDown: function (e) {
+      if ($(".menu-section__toggle").hasClass('on')) {
+        switch(e.keyCode) {
+          case 49: //1
+            location.href = $('.menu-section ul li:nth-child(1) a').attr('href');
+            break;
 
-      default: return; // exit this handler for other keys
+          case 50: //2
+            location.href = $('.menu-section ul li:nth-child(2) a').attr('href');
+            break;
+
+          case 51: //3
+            location.href = $('.menu-section ul li:nth-child(3) a').attr('href');
+            break;
+
+          case 52: //4
+            location.href = $('.menu-section ul li:nth-child(4) a').attr('href');
+            break;
+
+          case 53: //5
+            location.href = $('.menu-section ul li:nth-child(5) a').attr('href');
+            break;
+
+          default: return; // exit this handler for other keys
+          }
+        }
+        
+      else {
+        switch(e.keyCode) {
+          case 37: // left arr
+          if ($('.cs-main').length) {
+            location.href = $('.pagination .left a').attr('href');
+          }
+          break;
+          
+          case 39: // left arr
+          if ($('.cs-main').length) {
+            location.href = $('.pagination .right a').attr('href');
+          }
+          break;
+
+          case 77: //1
+             $('.menu-section__toggle').trigger('click');
+            break;
+
+          case 189: //+ slider
+            $('.slidesjs-previous').trigger('click');
+            break;
+
+          case 187: //+ slider
+            $('.slidesjs-next').trigger('click');
+            break;
+
+          case 191: //?
+            alert('m: open menu, then press 1-9 to navigate the menu, left arrow: previous project, right arrow: next project, -: prev. slide (slider), +: next slide (slider)');
+            break;
+          
+          default: return; 
+        }
       }
-    }
-    
-  //open menu
-  if (e.keyCode == 77) { 
-    $('.menu-section__toggle').trigger('click');
-  }
+    },
+  };
 
-  //slider with + & -
-  if (e.keyCode == 189) { 
-    $('.slidesjs-previous').trigger('click');
-  }
-  if (e.keyCode == 187) { 
-    $('.slidesjs-next').trigger('click');
-  }
+  PORT.common.init();
+})(jQuery, window, document);
 
-  // pagination if you're inside a case-study page
-  if ($('.cs-main').length) {
-    if (e.keyCode == 37) { 
-      location.href = $('.pagination .left a').attr('href');
-    }
-    else if (e.keyCode == 39) { 
-      location.href = $('.pagination .right a').attr('href');
-    }
-  }
 
-  // ? to tell you commands
-  if (e.keyCode == 191) { 
-  alert('m: open menu, then press 1-9 to navigate the menu, left arrow: previous project, right arrow: next project, L: prev. slide (slider), R: next slide (slider), +: next page (turn.js), -: previous page, ?: key');
-  }
-  
-  /* arrow keys:
-  37 - left
-  38 - up
-  39 - right
-  40 - down
-  76 - L
-  82 - R
-  77 - M
-  187 - + =
-  189 - - _
-  191 - /?
-  16 - shift
-  1 - 49
-  2 - 50
-  3 - 51
-  4 - 52
-  5 - 53
-  6 - 54
-  7 - 55
-  8 - 56
-  9 -57
-  */
-});
+      
+      /* arrow keys:
+      37 - left
+      38 - up
+      39 - right
+      40 - down
+      76 - L
+      82 - R
+      77 - M
+      187 - + =
+      189 - - _
+      191 - /?
+      16 - shift
+      1 - 49
+      2 - 50
+      3 - 51
+      4 - 52
+      5 - 53
+      6 - 54
+      7 - 55
+      8 - 56
+      9 -57
+      */
 
 // WAYPOINTS
 $('.js-fadeIn').waypoint(function() {
   $(this).css('opacity', '1');
 }, { offset: '25%' });
 
-//detect Chrome to fix ampersand bug bc firefox doesn't support unicode-range
+//detect Chrome to fix ampersand bug bc Firefox doesn't support unicode-range
 if(/chrom(e|ium)/.test(navigator.userAgent.toLowerCase())){
   $('html').addClass('chrome');
 }
@@ -154,5 +171,18 @@ $(document).ready(function(){
         $(".large").css({left: px, top: py, backgroundPosition: bgp});
       }
     }
-  })
+  }
+});
+
+
+//slider
+$('.slide').each(function () {
+  $('#slide-controls').append('<li></li>');
+}),
+
+$('#slide-controls li').each(function (index, item) {
+  $(item).on('click', function ()
+  {
+    $('.slidesjs-pagination li a').eq(index).trigger('click');
+  });
 });
