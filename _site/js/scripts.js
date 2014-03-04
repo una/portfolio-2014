2b37a6c09712604eba4c532054cca848f5d22dd9
Una Kravets <Una21136@yahoo.com>
2014-03-04 10:35:32 -0500

    js is looking a lot neater now

--- _site/js/scripts.js
+++ _site/js/scripts.js
@@ -13,8 +13,17 @@ window.PORT || (PORT = {});
     },
 
     bindEvents: function () {
-       $(".menu-section__toggle").on('click', this.menuSection);
-       $(document).on('keydown', this.keyDown);
+      this.detectChrome();
+      this.magnifyMe();
+      $(".menu-section__toggle").on('click', this.menuSection);
+      $(document).on('keydown', this.keyDown);
+    },
+
+    detectChrome: function () {
+      //detect Chrome to fix ampersand bug bc Firefox doesn't support unicode-range
+      if(/chrom(e|ium)/.test(navigator.userAgent.toLowerCase())){
+        $('html').addClass('chrome');
+      }
     },
 
     menuSection: function () {
@@ -83,97 +92,49 @@ window.PORT || (PORT = {});
         }
       }
     },
+
+    magnifyMe: function () {
+      var native_width = 0;
+      var native_height = 0;
+      $(".magnify").mousemove(function(e){
+        if(!native_width && !native_height)
+        {
+
+          var image_object = new Image();
+          image_object.src = $(".small").attr("src");
+          
+          native_width = image_object.width;
+          native_height = image_object.height;
+        }
+        else
+        {
+          var magnify_offset = $(this).offset();
+          var mx = e.pageX - magnify_offset.left;
+          var my = e.pageY - magnify_offset.top;    
+          if(mx < $(this).width() && my < $(this).height() && mx > 0 && my > 0)
+          {$(".large").fadeIn(100);}
+          else
+          {$(".large").fadeOut(100);}
+          if($(".large").is(":visible"))
+          {
+            var rx = Math.round(mx/$(".small").width()*native_width - $(".large").width()/2)*-1;
+            var ry = Math.round(my/$(".small").height()*native_height - $(".large").height()/2)*-1;
+            var bgp = rx + "px " + ry + "px";
+            var px = mx - $(".large").width()/2;
+            var py = my - $(".large").height()/2;
+            $(".large").css({left: px, top: py, backgroundPosition: bgp});
+          }
+        }
+      });
+    },
   };
 
   PORT.common.init();
 })(jQuery, window, document);
 
-
-      
       /* arrow keys:
-      37 - left
-      38 - up
-      39 - right
-      40 - down
-      76 - L
-      82 - R
-      77 - M
-      187 - + =
-      189 - - _
-      191 - /?
-      16 - shift
-      1 - 49
-      2 - 50
-      3 - 51
-      4 - 52
-      5 - 53
-      6 - 54
-      7 - 55
-      8 - 56
-      9 -57
+      37 - left, 38 - up, 39 - right, 40 - down, 76 - L, 82 - R, 77 - M, 187 - + =, 189 - - _, 191 - /?, 16 - shift, 1 - 49, 2 - 50, 3 - 51, 4 - 52, 5 - 53, 6 - 54, 7 - 55, 8 - 56, 9 -57
       */
-
-// WAYPOINTS
-$('.js-fadeIn').waypoint(function() {
-  $(this).css('opacity', '1');
-}, { offset: '25%' });
-
-//detect Chrome to fix ampersand bug bc Firefox doesn't support unicode-range
-if(/chrom(e|ium)/.test(navigator.userAgent.toLowerCase())){
-  $('html').addClass('chrome');
-}
-
-// FANNOTATE 
-$('.fannouts').waypoint(function() {
-  $(this).css('opacity', '1').addClass('opened');
-}, { offset: '25%' });
-$('.js-invert').waypoint(function() {
-  $(this).addClass('inverted');
-}, { offset: '50%' });
-
-// Lazy Load 
-$("img.lazy").lazyload({
-  effect : "fadeIn",
-  threshold : 200,
-  skip_invisible : false
-});
-
-//MAGNIFY
-$(document).ready(function(){
-  var native_width = 0;
-  var native_height = 0;
-  $(".magnify").mousemove(function(e){
-    if(!native_width && !native_height)
-    {
-
-      var image_object = new Image();
-      image_object.src = $(".small").attr("src");
-      
-      native_width = image_object.width;
-      native_height = image_object.height;
-    }
-    else
-    {
-      var magnify_offset = $(this).offset();
-      var mx = e.pageX - magnify_offset.left;
-      var my = e.pageY - magnify_offset.top;    
-      if(mx < $(this).width() && my < $(this).height() && mx > 0 && my > 0)
-      {$(".large").fadeIn(100);}
-      else
-      {$(".large").fadeOut(100);}
-      if($(".large").is(":visible"))
-      {
-        var rx = Math.round(mx/$(".small").width()*native_width - $(".large").width()/2)*-1;
-        var ry = Math.round(my/$(".small").height()*native_height - $(".large").height()/2)*-1;
-        var bgp = rx + "px " + ry + "px";
-        var px = mx - $(".large").width()/2;
-        var py = my - $(".large").height()/2;
-        $(".large").css({left: px, top: py, backgroundPosition: bgp});
-      }
-    }
-  }
-});
-
 
 //slider
 $('.slide').each(function () {
